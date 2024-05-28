81fdcbf14fc7a72d05becf3cdbe117aaf3221b3d
OKABESANA <okbsa7@gmail.com>
2024-05-28 19:08:56 +0900

    ああ

--- gramnista_LP/js/script.js
+++ gramnista_LP/js/script.js
@@ -1,13 +1,30 @@
 
-let windowSize = $(window).height(); //<- 画面の縦幅
+// let windowSize = $(window).height(); //<- 画面の縦幅
 
-// スクロールされるたびに発火する
+// // スクロールされるたびに発火する
+// $(window).scroll(function () {
+//     // 以下の処理で、現在のスクロール位置を求める
+//     let scrollPosition = $(this).scrollTop();
+//     // .offset().topでその要素がある位置を求めます。
+//     if (scrollPosition > $('.onayami--logo').offset().top - windowHeight) {
+//         // スクロール位置がアニメーションしたい要素の位置を超えたタイミングで、発火！
+//         $('onayami--logo').addClass('toUp , delay05 , duration01');
+//     }
+// });
+
+
+
 $(window).scroll(function () {
-    // 以下の処理で、現在のスクロール位置を求める
-    let scrollPosition = $(this).scrollTop();
-    // .offset().topでその要素がある位置を求めます。
-    if (scrollPosition > $('.onayami--logo').offset().top - windowHeight) {
-        // スクロール位置がアニメーションしたい要素の位置を超えたタイミングで、発火！
-        $('onayami--logo').addClass('toUp , delay05 , duration01');
-    }
+    
+    $('.toUp ,.toDown ,.leftUp ,.slideLeft ,.slideRight').each(function () {
+
+            let scrollPosition = $(window).scrollTop();
+            let windowHeight = $(window).height();
+            let elementTopPosition = $(this).offset().top;
+
+        if (scrollPosition + windowHeight > elementTopPosition) {
+            $(this).addClass('show');
+        }
+
+    });
 });
